4300b8e2676d04b425c160aa13cd5a63ca95fad8
IKROMOV DILSHOD <77174755+DeveloperBackendPro@users.noreply.github.com>
2022-02-25 14:06:20 +0500

    Update settings.py

--- ECOMMERCE/settings.py
+++ ECOMMERCE/settings.py
@@ -143,7 +143,7 @@ CKEDITOR_CONFIGS = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'leta19922@gmail.com'
+EMAIL_HOST_USER = '@gmail.com'
 EMAIL_HOST_PASSWORD = '************'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
